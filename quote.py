baf9c721906aa53d0fe320ef50f5932480233b0f
josephmani <josephmani.uae@gmail.com>
2021-06-24 21:31:38 +0530

    Python Web Crawler that scrapes and stores quotes in a psql database.

---
+++ quote.py
@@ -0,0 +1,49 @@
+import requests
+import bs4
+import sys
+import psycopg2
+
+def load_quotes():
+	dbconn = psycopg2.connect("dbname=quotes")
+	cursor = dbconn.cursor()
+	for i in range(1,30):
+		url= "https://www.goodreads.com/quotes/tag/inspirational?page="
+		page= url+ str(i)
+		resp=requests.get(page)
+		soup =bs4.BeautifulSoup(resp.content,features="html.parser")
+		quote_author =soup.find_all("div",attrs={"class":"quoteText"})
+		for i in quote_author:
+			texts= str(i.text.strip().split('―')[0])
+			author_name= str(i.text.strip().split('―')[1])
+		
+			texts=texts.replace('\n', '')
+			author_name=author_name.replace('\n','')
+			author_name=author_name.replace('   ','')
+		
+			cursor.execute("INSERT INTO openings (quote, author) VALUES(%s, %s)",(texts,author_name))
+		dbconn.commit()
+
+
+
+def create_db():
+	dbconn = psycopg2.connect("dbname=quotes")
+	cursor = dbconn.cursor()
+	f=open("words.sql")
+	sql_code= f.read()
+	f.close()
+	cursor.execute(sql_code)
+	dbconn.commit()
+	
+	
+def main(arg):
+	if arg=="create":
+		create_db()
+	elif arg=="crawl":
+		load_quotes()
+	else:
+		print(f"Unknown command {arg}")		
+
+
+if __name__=="__main__": #import guard
+	main(sys.argv[1])
+	 
