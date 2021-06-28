52b2bd12b882098ea8161acd6b91b477b67fb791
josephmani <josephmani.uae@gmail.com>
2021-06-28 22:29:11 +0530

    Updates quote.py and sets length of quote to 100 characters.

--- quote.py
+++ quote.py
@@ -6,7 +6,7 @@ import psycopg2
 def load_quotes():
 	dbconn = psycopg2.connect("dbname=quotes")
 	cursor = dbconn.cursor()
-	for i in range(1,30):
+	for i in range(1,100):
 		url= "https://www.goodreads.com/quotes/tag/inspirational?page="
 		page= url+ str(i)
 		resp=requests.get(page)
@@ -16,11 +16,22 @@ def load_quotes():
 			texts= str(i.text.strip().split('―')[0])
 			author_name= str(i.text.strip().split('―')[1])
 		
-			texts=texts.replace('\n', '')
-			author_name=author_name.replace('\n','')
-			author_name=author_name.replace('   ','')
-		
-			cursor.execute("INSERT INTO openings (quote, author) VALUES(%s, %s)",(texts,author_name))
+			texts=texts.replace('“','')
+			texts=texts.replace('”','')
+			if len(texts)>100:
+				continue
+			
+			author_name = author_name.replace('\n','')
+			author_name = author_name.replace('   ','')
+			
+			if ", " in author_name:
+				a_list = author_name.split(", ")
+				a_list[1]=","+a_list[1]
+				cursor.execute("INSERT INTO openings (quote, author, source) VALUES(%s, %s, %s)",(texts, a_list[0], a_list[1]))
+			
+			else:
+				cursor.execute("INSERT INTO openings (quote, author) VALUES(%s, %s)",(texts, author_name))			
+			
 		dbconn.commit()
 
 
